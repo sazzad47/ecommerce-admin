c2e27c9472e37f3b2199be2505caca15c8b87bcb
Sazzad Hossen <sazzad47.ju@gmail.com>
2023-09-10 15:02:15 +0600

    make it better

--- src/pages/Product/Form/ProductForm.jsx
+++ src/pages/Product/Form/ProductForm.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, createContext, useState } from "react";
-import { Form, Button } from "antd";
+import React, { useEffect, createContext, useState, useRef } from "react";
+import { Form } from "antd";
 import { useForm } from "react-hook-form";
 import { CategoryHooks } from "../../../Features";
 import ProductProps from "./ProductProps";
 import ProductDescription from "./ProductDescription";
-import { ProductHooks ,  } from "../../../Features";
+import { ProductHooks } from "../../../Features";
 import { Oval } from "react-loader-spinner";
-import { MenuItem, Select, TextField } from "@mui/material";
+import { Button, MenuItem, Select, TextField } from "@mui/material";
+import { FiUpload } from "react-icons/fi";
 export const ProductFormContext = createContext();
 
 const ProductForm = () => {
+  const uploadInput = useRef(null);
   const { useCategory } = CategoryHooks;
   const { useAddProduct } = ProductHooks;
   const { CategoryData, CategoryLoading, GetCategory } = useCategory();
@@ -27,10 +29,16 @@ const ProductForm = () => {
     formState: { errors },
   } = useForm();
 
+  const productImages = Array.from(watch("product_images") || []);
   const category = watch("category_id");
+
+  const handleUplaod = () => {
+    uploadInput.current?.click();
+  };
 
   const onSubmit = (data) => {
     preAdding({ ...data, ...addtional });
+    console.log("additional", addtional);
   };
 
   useEffect(() => {
@@ -42,6 +50,9 @@ const ProductForm = () => {
     if (ProductAddSuccess) {
       reset();
     }
+
+    // Assign the ref value once the component is mounted
+    uploadInput.current = document.getElementById("uploadInput");
   }, [ProductAddLoading, ProductAddSuccess]);
 
   return (
@@ -60,7 +71,10 @@ const ProductForm = () => {
               validateStatus={errors.name ? "error" : ""}
               help={errors.name && errors.name.message}
             >
-              <TextField variant="standard" {...register("name", { required: "Name is required" })} />
+              <TextField
+                variant="standard"
+                {...register("name", { required: "Name is required" })}
+              />
             </Form.Item>
             <Form.Item
               label="Category"
@@ -69,7 +83,7 @@ const ProductForm = () => {
             >
               {!CategoryLoading && (
                 <Select
-                 variant="standard"
+                  variant="standard"
                   style={{ width: "200px" }}
                   {...register("category_id", {
                     required: "Category is required",
@@ -94,7 +108,7 @@ const ProductForm = () => {
               }
             >
               <TextField
-               variant="standard"
+                variant="standard"
                 {...register("short_description", {
                   required: "Short Description is required",
                 })}
@@ -102,7 +116,7 @@ const ProductForm = () => {
             </Form.Item>
           </div>
           <div className="d-flex a-center g-15">
-          <Form.Item
+            <Form.Item
               label="Price"
               validateStatus={errors.price ? "error" : ""}
               help={errors.price && errors.price.message}
@@ -110,7 +124,6 @@ const ProductForm = () => {
               <TextField
                 variant="standard"
                 type="number"
-             
                 {...register("price", { required: "Price is required" })}
               />
             </Form.Item>
@@ -130,13 +143,12 @@ const ProductForm = () => {
               validateStatus={errors.discount ? "error" : ""}
               help={errors.discount && errors.discount.message}
             >
-             <TextField
+              <TextField
                 variant="standard"
                 type="number"
                 {...register("discount", { required: "Discount is required" })}
               />
             </Form.Item>
-           
           </div>
 
           <div className="d-flex g-15">
@@ -172,6 +184,8 @@ const ProductForm = () => {
               help={errors.product_images && errors.product_images.message}
             >
               <input
+                hidden
+                id="uploadInput"
                 type="file"
                 multiple
                 accept="image/*"
@@ -179,6 +193,24 @@ const ProductForm = () => {
                   required: "Product Images are required",
                 })}
               />
+              <Button
+                startIcon={<FiUpload />}
+                variant="outlined"
+                onClick={() => handleUplaod()}
+              >
+                Choose Images
+              </Button>
+              {productImages.length > 0 &&
+                productImages.map((file, index) => (
+                  <div key={index} style={{margin: "5px 0", display: "flex", alignItems: "center", gap: "4px"}} >
+                    <img
+                      src={URL.createObjectURL(file)}
+                      alt={`Selected Image ${index}`}
+                      style={{ width: "50px", height: "50px" }}
+                    />
+                    <span>{file.name}</span>
+                  </div>
+                ))}
             </Form.Item>
           </div>
         </div>
