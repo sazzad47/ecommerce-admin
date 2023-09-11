3f31a02e99cb3d73f5976c030201f0029628e914
Sazzad <sazzad47.ju@gmail.com>
2023-09-11 12:31:57 +0600

    make it better

--- src/pages/Properties/PropForm.jsx
+++ src/pages/Properties/PropForm.jsx
@@ -3,6 +3,7 @@ import { Form, Button } from "antd";
 import { useForm } from "react-hook-form";
 import { PropHooks } from "../../Features";
 import { Oval } from "react-loader-spinner";
+import { Grid } from "@mui/material";
 
 const PropForm = () => {
   const {
@@ -27,41 +28,50 @@ const PropForm = () => {
 
   return (
     <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
-      <div className="d-flex a-center g-15 w-100">
-        <Form.Item
-          label="Name"
-          validateStatus={errors.name ? "error" : ""}
-          help={errors.name && errors.name.message}
-        >
-          <input {...register("name", { required: "Name is required" })} />
-        </Form.Item>
-        <Form.Item
-          label="Type"
-          validateStatus={errors.type ? "error" : ""}
-          help={errors.type && errors.type.message}
-        >
-          <select
-            style={{ width: "200px" }}
-            {...register("type", { required: "Type is required" })}
+      <Grid container spacing={2}>
+        <Grid item sx={12} md={6} style={{width: "100%"}}>
+          <Form.Item
+            label="Name"
+            validateStatus={errors.name ? "error" : ""}
+            help={errors.name && errors.name.message}
           >
-            <option value="TEXT">text</option>
-          </select>
-        </Form.Item>
-        <Form.Item
-          label="Values Seperate between every value with (,)"
-          validateStatus={errors.values ? "error" : ""}
-          help={errors.values && errors.values.message}
-        >
-          <input
-            style={{ width: "400px" }}
-            placeholder="Seperate between every value with (,)"
-            {...register("values", {
-              required: "Values are required",
-              setValueAs: (val) => val.split(","),
-            })}
-          />
-        </Form.Item>
-      </div>
+            <input
+              style={{ width: "100%" }}
+              {...register("name", { required: "Name is required" })}
+            />
+          </Form.Item>
+        </Grid>
+        <Grid style={{width: "100%"}} item sx={12} md={6}>
+          <Form.Item
+            label="Type"
+            validateStatus={errors.type ? "error" : ""}
+            help={errors.type && errors.type.message}
+          >
+            <select
+              style={{ width: "100%" }}
+              {...register("type", { required: "Type is required" })}
+            >
+              <option value="TEXT">text</option>
+            </select>
+          </Form.Item>
+        </Grid>
+        <Grid item sx={12} md={6} style={{width: "100%"}}>
+          <Form.Item
+            label="Values Seperate between every value with (,)"
+            validateStatus={errors.values ? "error" : ""}
+            help={errors.values && errors.values.message}
+          >
+            <input
+              style={{ width: "100%" }}
+              placeholder="Seperate between every value with (,)"
+              {...register("values", {
+                required: "Values are required",
+                setValueAs: (val) => val.split(","),
+              })}
+            />
+          </Form.Item>
+        </Grid>
+      </Grid>
 
       <Form.Item>
         <Button type="primary" htmlType="submit" disabled={PropAddLoading}>
